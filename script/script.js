839d2028cef53e2a98ff663ec01bd9e1ab3a34a5
Gregar <126393669+mrd010@users.noreply.github.com>
2023-10-11 23:46:49 +0330

    Add sliding side nav bar for mobile

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
+// favorite button action - it toggles favorite on projects
 const favBtns = document.querySelectorAll(".projects-section .fav-button");
 
 favBtns.forEach((btn) => {
@@ -10,4 +11,44 @@ function toggleProjectButton() {
   } else {
     this.classList.add("on");
   }
+}
+
+const sideMenu = document.querySelector(".side-nav");
+const sideMenuBackdrop = document.querySelector(".nav-backdrop");
+const menuBtn = document.querySelector(".header .hamburger-button");
+const closeBtn = sideMenu.querySelector(".close-button");
+
+let sideMenuIsOpen = false;
+
+// side menu on mobile close side menu with close button
+closeBtn.addEventListener("click", closeMenu);
+
+sideMenuBackdrop.addEventListener("click", (e) => {
+  if (!sideMenuIsOpen) {
+    return;
+  }
+  const target = e.target;
+  if (!target.closest(".side-nav")) {
+    closeMenu();
+  }
+});
+
+function closeMenu() {
+  sideMenu.style.left = "calc(0px - 100vw)";
+  sideMenuBackdrop.classList.remove("blurred");
+  sideMenuBackdrop.classList.add("transparent");
+  document.body.style.touchAction = "initial";
+  sideMenuIsOpen = false;
+}
+
+// side menu on mobile open side menu with menu button
+
+menuBtn.addEventListener("click", openMenu);
+
+function openMenu() {
+  document.body.style.touchAction = "none";
+  sideMenu.style.left = "0";
+  sideMenuBackdrop.classList.remove("transparent");
+  sideMenuBackdrop.classList.add("blurred");
+  sideMenuIsOpen = true;
 }
